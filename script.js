69476bceded1198030994ad0f87337c5ac1704ce
Ess <ess.a.ha101@gmail.com>
2026-08-22 11:01:57 +0100

    initial commit innit

---
+++ script.js
@@ -0,0 +1,11 @@
+const happyCat = document.getElementById("happy-cat")
+
+
+function gayClick() {
+    document.getElementById("yes").innerHTML = "Yippee!";
+    document.getElementById("happy-cat").style.display = "block";
+}
+
+function notgayClick() {
+    document.getElementById("no").innerHTML = "Boo :(";
+}
