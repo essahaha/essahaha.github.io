0459b93e50c7f7ea447e46af45f0cbc6573e0baf
Ess <ess.a.ha101@gmail.com>
2026-09-07 17:28:41 +0100

    othercopywriting

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 function openCarousel() {
-    document.getElementById("carousel-overlay").classList.add("open");
+    document.getElementById("copy-carousel-overlay").classList.add("open");
 }
 
 function closeCarousel() {
-    document.getElementById("carousel-overlay").classList.remove("open");
+    document.getElementById("copy-carousel-overlay").classList.remove("open");
+}
+
+function openfavCarousel() {
+    document.getElementById("favourites-carousel-overlay").classList.add("open");
+}
+
+function closefavCarousel() {
+    document.getElementById("favourites-carousel-overlay").classList.remove("open");
+}
+
+function openotherCarousel() {
+    document.getElementById("other-carousel-overlay").classList.add("open");
+}
+
+function closeotherCarousel() {
+    document.getElementById("other-carousel-overlay").classList.remove("open");
 }
